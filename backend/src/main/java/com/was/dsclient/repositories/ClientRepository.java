194673b9f6b75eec109ba87ed9931b378f767112
waner <wanersbh@gmail.com>
2021-08-05 19:08:42 -0300

    Rename package repository to repositories

--- backend/src/main/java/com/was/dsclient/repositories/ClientRepository.java
+++ backend/src/main/java/com/was/dsclient/repositories/ClientRepository.java
@@ -1,4 +1,4 @@
-package com.was.dsclient.repository;
+package com.was.dsclient.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
