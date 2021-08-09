1e7960e872014428a9b2799d01fe84d95de46b32
waner <wanersbh@gmail.com>
2021-08-09 08:09:20 -0300

    created findById method.

--- backend/src/main/java/com/was/dsclient/resources/ClientResource.java
+++ backend/src/main/java/com/was/dsclient/resources/ClientResource.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,6 +36,11 @@ public class ClientResource {
 		return ResponseEntity.ok(list);
 	}
 	
-	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
+		ClientDTO dto = service.findById(id);
+		
+		return ResponseEntity.ok().body(dto);
+	}
 
 }
