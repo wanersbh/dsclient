ba8bf13632133374cec36039818c915c0a513884
waner <wanersbh@gmail.com>
2021-08-09 08:05:21 -0300

    created resources

---
+++ backend/src/main/java/com/was/dsclient/resources/ClientResource.java
@@ -0,0 +1,40 @@
+package com.was.dsclient.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.was.dsclient.dto.ClientDTO;
+import com.was.dsclient.services.ClientService;
+
+@RestController
+@RequestMapping(value = "/clients")
+public class ClientResource {
+
+	@Autowired
+	private ClientService service;
+	
+	@GetMapping
+	public ResponseEntity<Page<ClientDTO>> findAll(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+			){
+		
+		PageRequest request = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		Page<ClientDTO> list = service.findAllPaged(request);
+		
+		return ResponseEntity.ok(list);
+	}
+	
+	
+
+}
